dde873d091756a55da6e38d043212011558f562e
zilixie <zili.xie@mail.utoronto.ca>
2017-10-21 15:46:33 -0400

    Update sr_router.c

--- router/sr_router.c
+++ router/sr_router.c
@@ -170,11 +170,9 @@ void handle_ip(struct sr_instance* sr,
 				unsigned int len,
 				char* interface/* lent */)
 {
-	int etnet_hdr_size = sizeof(sr_ethernet_hdr_t);
-	int ip_hdr_size = sizeof(sr_ip_hdr_t);
 	sr_ip_hdr_t* ip_hdr = (sr_ip_hdr_t *)(packet + etnet_hdr_size);
-	
-	if (len < etnet_hdr_size + ip_hdr_size) {
+
+	if (len < sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)) {
 		printf("invalid datagram length\n");
 		return;
 	}
@@ -183,41 +181,46 @@ void handle_ip(struct sr_instance* sr,
 		printf("invalid ip packet cksum\n");
 		return;	
 	}
-	ip_hdr->ip_ttl--;
 
-	if(ip_hdr->ip_ttl == 0) {
-		/*send icmp time exceeded*/
-		printf("no ttl remaining.\n");
-		/*handle_icmp(sr, 11, 0, packet, len, interface); */
-	}else {
-		/* router is not the receiver*/
-		if (ip_in_sr_interface_list(sr, ip_hdr->ip_dst)) {
-			struct sr_rt *rt_entry = rt_entry_lpm(sr, ip_hdr->ip_dst);
-			if (rt_entry != NULL) {
-				/*outgoing interface*/
-				struct sr_if *sender_interface_pt = sr_get_interface(sr, rt_entry->interface);
+	/* router is not the receiver*/
+	if (!ip_in_sr_interface_list(sr, ip_hdr->ip_dst)) {
 
-				/* look up the cache to find arpentry*/
-				struct sr_arpentry *arp_entry = sr_arpcache_lookup(&sr->cache, rt_entry->gw.s_addr);
+		ip_hdr->ip_ttl--;
+		if (ip_hdr->ip_ttl == 0) {
+			handle_icmp_t11_pkt();
+		}
 
-				/*not found*/
-				if (arp_entry == NULL) {
-					/*add to the arp queue, and send a arp request*/
-					struct sr_arpreq * arp_req = sr_arpcache_queuereq(&sr->cache, ip_hdr->ip_dst, packet, len, sender_interface_pt->name);
-					/*handle_arpreq(sr, arp_req);*/
-					return;
-				}
-				else if (arp_entry != NULL){
+		struct sr_rt *rt_entry = rt_entry_lpm(sr, ip_hdr->ip_dst);
 
-					/*prepare for ethernet header*/
-					sr_ethernet_hdr_t * etnet_hdr;
-					etnet_hdr = (sr_ethernet_hdr_t *)packet;
+		if (rt_entry != NULL) {
+			//outgoing interface
+			struct sr_if *sender_interface_pt = sr_get_interface(sr, rt_entry->interface);
 
-					replace_etnet_addrs(etnet_hdr, sender_interface_pt->addr, arp_entry->mac);
-					sr_send_packet(sr, packet, len, sender_interface_pt->name);
-				}
+			/* look up the cache to find arpentry*/
+			struct sr_arpentry *arp_entry = sr_arpcache_lookup(&sr->cache, rt_entry->gw.s_addr);
+
+			/*not found*/
+			if (arp_entry == NULL) {
+				/*add to the arp queue, and send a arp request*/
+				struct sr_arpreq * arp_req = sr_arpcache_queuereq(&sr->cache, ip_hdr->ip_dst, packet, len, out_if->name);
+				handle_arpreq(sr, arp_req);
+				return;
+			}
+
+			else if (arp_entry != NULL){
+
+				/*prepare for ethernet header*/
+				sr_ethernet_hdr_t * etnet_hdr;
+				etnet_hdr = (sr_ethernet_hdr_t *)packet;
+
+				replace_etnet_addrs(sender_interface_pt->addr, arp_entry->mac);
+				sr_send_packet(sr, packet, len, sender_interface_pt->name);
+				return;
 			}
 		}
+
+	} else {
+
 	}
 }
 

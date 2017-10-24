45ae0fb73720e72a92ec7845c0bd34d65ce4b7ad
zilixie <zili.xie@mail.utoronto.ca>
2017-10-23 22:57:52 -0400

    Update sr_arpcache.c

--- router/sr_arpcache.c
+++ router/sr_arpcache.c
@@ -31,45 +31,67 @@ void open_arp_req(struct sr_instance* sr, struct sr_arpreq * req) {
     	arp_hdr->ar_sip = dst_interface->ip;
 
 	
-    
 	uint8_t broadcast_addr[ETHER_ADDR_LEN]  = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+	etnet_hdr->ether_type = htons(ethertype_arp);
     	replace_arp_hardware_addrs(arp_hdr, dst_interface->addr, broadcast_addr);
-
-
     	replace_etnet_addrs(etnet_hdr, dst_interface->addr, broadcast_addr);
-   	etnet_hdr->ether_type = htons(ethertype_arp);
     	sr_send_packet(sr, pkt, arp_hdr_size + etnet_hdr_size, dst_interface->name);
+	
     	free(pkt);
 }
 
 
 
-void handle_arpreq(struct sr_instance *sr, struct sr_arpreq* arp_req){
+void handle_arpreq(struct sr_instance *sr, struct sr_arpreq* req){
     	struct sr_arpcache *cache = &(sr->cache);
     	/*struct sr_if *currIface;*/
     	time_t current_time = time(0);
-    	time_t last_sent = arp_req->sent;
-    	uint32_t tiems_sent = arp_req->times_sent;
+    	time_t last_sent = req->sent;
+    	uint32_t times_sent = req->times_sent;
     	struct sr_packet *pkt_pt;
 
 
     	if (difftime(current_time, last_sent) > 1.0) {
-        	if (tiems_sent >= 5) {
-            		pkt_pt = arp_req->packets;          
-           		 while (pkt_pt) {
+        	if (times_sent >= 5) {
+            		pkt_pt = req->packets;          
+           		while (pkt_pt != NULL) {
                 		/* Send type 3 code 1 ICMP (Host Unreachable) */
                 		send_icmp_t3_pkt(sr, pkt_pt->buf, pkt_pt->iface, pkt_pt->len, 3, 1);
                 		pkt_pt = pkt_pt->next;
             		}
             		/* destroy the request */
-            		sr_arpreq_destroy(cache, arp_req);
+            		sr_arpreq_destroy(cache, req);
             
         	} else {
             		/* open an arp request */
-			open_arp_req(sr, arp_req);
+			int arp_hdr_size = sizeof(sr_arp_hdr_t);
+			int etnet_hdr_size = sizeof(sr_ethernet_hdr_t);
+
+			struct sr_if *dst_interface = sr_get_interface(sr, req->packets->iface);
+			uint8_t * pkt = (uint8_t *)malloc(arp_hdr_size + etnet_hdr_size);
+			sr_ethernet_hdr_t * etnet_hdr = (sr_ethernet_hdr_t *)pkt;
+			sr_arp_hdr_t * arp_hdr = (sr_arp_hdr_t *)(pkt + sizeof(sr_ethernet_hdr_t));	
+
+
+			arp_hdr->ar_op = htons(arp_op_request);
+			arp_hdr->ar_hrd = htons(arp_hrd_ethernet);
+			arp_hdr->ar_pro = htons(ethertype_ip);
+			arp_hdr->ar_hln = ETHER_ADDR_LEN;
+			arp_hdr->ar_pln = sizeof(uint32_t);
+			arp_hdr->ar_tip = req->ip;
+			arp_hdr->ar_sip = dst_interface->ip;
+
+
+			uint8_t broadcast_addr[ETHER_ADDR_LEN]  = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+			etnet_hdr->ether_type = htons(ethertype_arp);
+			replace_arp_hardware_addrs(arp_hdr, dst_interface->addr, broadcast_addr);
+			replace_etnet_addrs(etnet_hdr, dst_interface->addr, broadcast_addr);
+			sr_send_packet(sr, pkt, arp_hdr_size + etnet_hdr_size, dst_interface->name);
+
+			free(pkt);
         	}
-	        arp_req->sent = current_time;
-            	arp_req->times_sent++;
+	        req->sent = current_time;
+            	req->times_sent++;
     	}
 }
 
@@ -83,7 +105,7 @@ void sr_arpcache_sweepreqs(struct sr_instance *sr) {
     struct sr_arpreq *req = arpcache->requests;
     struct sr_arpreq *req_cp;
 
-    while(req) {
+    while(req != NULL) {
 	req_cp = req;
         req = req->next;
         handle_arpreq(sr, req_cp);
